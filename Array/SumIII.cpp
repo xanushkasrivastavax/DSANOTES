9abde93a43498211b9b997453de4e5ceccd7d845
xanushkasrivastavax <43552277+xanushkasrivastavax@users.noreply.github.com>
2021-07-29 21:49:39 +0530

    Create SumIII.cpp

---
+++ Array/SumIII.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+       unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++)
+            mp[nums[i]]++;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mp[nums[i]]==1)
+                ans.push_back(nums[i]);
+        }
+        return ans;
+    }
+};

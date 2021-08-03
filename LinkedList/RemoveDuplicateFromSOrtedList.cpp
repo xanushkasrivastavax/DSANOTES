3cc67693ce117ef452d5471138b0fbde53689fba
Anushka Srivastava <43552277+xanushkasrivastavax@users.noreply.github.com>
2021-08-04 00:36:26 +0530

    Create RemoveDuplicateFromSOrtedList.cpp

---
+++ LinkedList/RemoveDuplicateFromSOrtedList.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* current = head;
+        ListNode* next_next ;
+        
+        if(current == NULL){
+            return NULL;
+        }
+        
+        while(current -> next != NULL){
+            if(current -> val == current -> next -> val){
+                current -> next = current -> next ->next;
+                current -> next = next_next;
+            }
+            else current = current -> next ; 
+        }
+        return head ;
+    }
+};
